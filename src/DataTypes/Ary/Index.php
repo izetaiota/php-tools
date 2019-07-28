ab6037617f71e9703a002e028010531860a229f0
anly <anly95@foxmail.com>
2019-07-28 18:14:02 +0800

    init

---
+++ src/DataTypes/Ary/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @description
+ * @author    [zetaiota]
+ * @since     2019/7/28
+ * @copyright
+ */
+
+namespace DataTypes\Ary;
+
+class Index
+{
+    public function test1()
+    {
+        var_dump(999);
+    }
+}
---
+++ src/DataTypes/String/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @description
+ * @author    [zetaiota]
+ * @since     2019/7/28
+ * @copyright
+ */
+
+namespace DataTypes\String;
+
+class Index
+{
+    public function test()
+    {
+        var_dump(666);
+    }
+}
