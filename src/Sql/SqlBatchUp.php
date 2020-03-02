20743866335687c1fb4a1899bbaf90c62509ef62
Anly <anly95@foxmail.com>
2020-03-02 14:00:01 +0800

    批量更新sql数据结构变更

--- src/Sql/SqlBatchUp.php
+++ src/Sql/SqlBatchUp.php
@@ -12,21 +12,22 @@ class SqlBatchUp
      * @description 批量更新sql组装优化语句 （参考资料：https://www.cnblogs.com/ldj3/p/9288187.html）
      * @modify
      *
-     * @param  string $table    表
-     * @param  array  $data     批量更新的数据，注意是二维数组
-     * @param  string $field    跟新的条件字段
-     * @param array   $whereAry 跟新的条件 数组
+     * @param  string $table         表
+     * @param  array  $data          批量更新的数据，注意是二维数组
+     * @param  array  $whereAry      更新数据数组
+     * @param  string $field         更新的条件字段
+     * @param array   $queryWhereAry 更新的条件 数组
      *
      * @return bool|string
      */
-    public function batchUpdate($table, $data, $field, $whereAry = [])
+    public function batchUpdate($table, $data, $whereAry, $field, $queryWhereAry = [])
     {
-        if (!is_array($data) || !$field || !is_array($whereAry)) {
+        if (!is_array($data) || !$field || !is_array($queryWhereAry) || !is_array($whereAry)) {
             return false;
         }
 
-        $updates = $this->parseUpdate($data, $field);
-        $where   = $this->parseParams($whereAry);
+        $updates = $this->parseUpdate($data, $whereAry, $field);
+        $where   = $this->parseParams($queryWhereAry);
 
         // 获取所有键名为$field列的值，值两边加上单引号，保存在$fields数组中
         $fields = array_column($data, $field);
@@ -41,23 +42,22 @@ class SqlBatchUp
     /**
      * 将二维数组转换成CASE WHEN THEN的批量更新条件
      *
-     * @param $data  array 二维数组
-     * @param $field string 列名
+     * @param  array  $data     array 二维数组
+     * @param array   $whereAry 更新的条件
+     * @param  string $field    列名
      *
      * @return string sql语句
      */
-    private function parseUpdate($data, $field)
+    private function parseUpdate($data, $whereAry, $field)
     {
-        $sql  = '';
-        $keys = array_keys(current($data));
-        foreach ($keys as $column) {
+        $sql = '';
+        $sql .= sprintf("`%s` = CASE `%s` \n", array_keys($whereAry)[0], $field);
 
-            $sql .= sprintf("`%s` = CASE `%s` \n", $column, $field);
-            foreach ($data as $line) {
-                $sql .= sprintf("WHEN '%s' THEN '%s' \n", $line[$field], $line[$column]);
-            }
-            $sql .= "END,";
+        foreach ($data as $line) {
+            $sql .= sprintf("WHEN '%s' THEN '%s' \n", $line[$field], array_values($whereAry)[0]);
         }
+
+        $sql .= "END,";
 
         return rtrim($sql, ',');
     }
