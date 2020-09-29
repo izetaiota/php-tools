7f7a09d50ccbede54d5daf7197a31567fbc1f043
zetaiota <anly95@foxmail.com>
2020-09-29 20:34:51 +0800

    添加年月日，年月区间范围函数

--- src/Util/Time.php
+++ src/Util/Time.php
@@ -17,4 +17,40 @@ class Time
 
     }
 
+
+    /**
+     * @Desc 创建年月日时间区间范围
+     * @param $ymdStart
+     * @param bool $ymdEnd
+     * @param int $range
+     * @return array|false[]|string[]
+     */
+    public static function createYmdRange($ymdStart, $ymdEnd = true, $range = 86400)
+    {
+        if ($ymdEnd === true) $ymdEnd = date('Y-m-d');
+        return array_map(function ($time) {
+            return date('Y-m-d', $time);
+        }, range(strtotime($ymdStart), strtotime($ymdEnd), $range));
+    }
+
+
+    /**
+     * @Desc 获取年月到当今前一个月的区间
+     * @param $startMon 2019-09
+     * @return array
+     */
+    public static function createYmInterval($startMon)
+    {
+        $startTime = strtotime($startMon); // 开始时间
+        $endTime = strtotime(date("Y-m", strtotime("-1 month"))); //结束时间
+
+        $monArr = array();
+        $monArr[] = $startMon; // 默认将开始时间添加进去，有需要的情况下自己更改;
+        while (($startTime = strtotime('+1 month', $startTime)) <= $endTime) {
+            $monArr[] = date('Y-m', $startTime); // 取得递增月;
+        }
+
+        return $monArr;
+    }
+
 }
